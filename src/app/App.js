74e402fcdc519b6e11038a407776cf86dcb91d15
Nathan Garcia <n8g423@gmail.com>
2023-12-07 23:06:44 -0700

    Added tailwind styling to more closely reflect the wireframe

--- src/app/App.js
+++ src/app/App.js
@@ -1,62 +1,87 @@
-import 'app/App.css';
-import Activity from 'pages/activity';
-import { useState } from 'react';
-import { users } from 'utils/data';
+import "app/App.css";
+import Activity from "pages/activity";
+import { useState } from "react";
+import { users } from "utils/data";
 
 function App() {
+  const inputStyle =
+    "px-4 py-3 my-2 rounded-full bg-gray-200 placeholder-gray-500";
+  const btnStyle = "px-6 py-4 my-5 rounded-full bg-black text-white text-bold";
 
-	const inputStyle = 'px-3 py-2 my-1 rounded-md';
+  const [user, setUser] = useState("");
+  const [pswd, setPswd] = useState("");
 
-	const [user, setUser] = useState('');
-	const [pswd, setPswd] = useState('');
+  const [logged, setLogged] = useState(false);
 
-	const [logged, setLogged] = useState(false)
+  const verify_Login = (user, pswd) => {
+    let response = Object.values(users).filter(
+      (e) => e.user === user && e.pswd === pswd
+    );
 
-	const verify_Login = (user, pswd) => {
+    console.log(response);
 
-		let response = Object.values( users ).filter(e => e.user === user && e.pswd === pswd)
+    if (response.length > 0) {
+      console.log("login successful");
+      setLogged(true);
+      return true;
+    } else {
+      console.log("login failed");
+      return false;
+    }
+  };
 
-		console.log(response);
+  const render_Login = () => {
+    return (
+      <div className={"flex flex-col w-96 m-10"}>
+        <div className={"flex flex-col gap-1"}>
+          <p className={"flex justify-start"}> {"Name"} </p>
+          <input
+            className={inputStyle}
+            type={"text"}
+            placeholder={"Enter your name"}
+            onInput={(e) => setUser(e.target.value)}
+          />
+          <p className={"flex justify-start"}> {"Password"} </p>
+          <input
+            className={inputStyle}
+            type={"text"}
+            placeholder={"Enter your password"}
+            onInput={(e) => setPswd(e.target.value)}
+          />
+          <button
+            className={btnStyle}
+            onMouseDown={() => verify_Login(user, pswd)}
+          >
+            {" "}
+            {"Login"}{" "}
+          </button>
+        </div>
+      </div>
+    );
+  };
 
-		if (response.length > 0) {
-			console.log("login successful")
-			setLogged(true)
-			return true
-		} else {
-			console.log("login failed")
-			return false
-		}
-	}
+  const render_Main = () => {
+    if (!logged) {
+      return render_Login();
+    } else {
+      return <Activity />;
+    }
+  };
 
-	const render_Login = () => {
-		return (
-			<div className={'flex flex-col w-64'}>
+  return (
+    <main
+      className={
+        "App bg-slate-50 min-h-screen flex flex-col justify-start items-center gap-4 py-6"
+      }
+    >
+      <h1 className={"font-bold text-2xl my-4"}>
+        {" "}
+        {"CAIL Project - Prototype"}{" "}
+      </h1>
 
-				<h3> {'Login'} </h3>
-
-				<div className={'flex flex-col gap-1'}>
-					<input className={inputStyle} type={'text'} placeholder={'username'} onInput={e => setUser(e.target.value)} />
-					<input className={inputStyle} type={'text'} placeholder={'password'} onInput={e => setPswd(e.target.value)} />
-					<button className={inputStyle + ' bg-slate-200'} onMouseDown={() => verify_Login(user, pswd)}> {'Login'} </button>
-				</div>
-
-			</div>
-		)
-	}
-
-	const render_Main = () => {
-		if (!logged) { return render_Login() } else { return <Activity /> }
-	}
-
-	return (
-		<main className={'App bg-slate-200 min-h-screen flex flex-col justify-start items-center gap-4 py-6'}>
-
-			<h1 className={'font-bold'}> {'CAIL Project - Prototype'} </h1>
-
-			{render_Main()}
-
-		</main>
-	);
+      {render_Main()}
+    </main>
+  );
 }
 
 export default App;
--- src/pages/activity.jsx
+++ src/pages/activity.jsx
@@ -1,78 +1,132 @@
 // Activity Page
 
-import ItemList from "components/item-list/item-list"
-import { useState } from "react"
-import { activities } from "utils/data"
+import ItemList from "components/item-list/item-list";
+import { useState } from "react";
+import { activities } from "utils/data";
 
 function Activity() {
+  const btnStyle =
+    "px-6 py-4 my-6 rounded-full bg-black text-white text-bold w-96";
 
-	const btnStyle = 'px-4 py-2 bg-slate-700 text-slate-100 font-bold mx-4 my-2 rounded-md'
+  const inputStyle =
+    "px-4 py-3 my-2 rounded-full bg-gray-200 placeholder-gray-500";
 
-	const inputStyle = 'px-3 py-2 my-1 rounded-md';
+  const [add, setAdd] = useState(false);
+  const [list, setList] = useState(activities);
 
-	const [add, setAdd ] = useState( false )
-	const [ list, setList ] = useState( activities )
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [studentID, setStudentID] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [descp, setDescp] = useState("");
+  const [approved, setApproved] = useState(false);
+  const [completed, setCompleted] = useState(false);
 
-	const [title, 		setTitle] = useState('')
-	const [type,		setType] = useState('')
-	const [studentID,	setStudentID] = useState('')
-	const [date, 		setDate] = useState(new Date())
-	const [descp, 		setDescp] = useState('')
-	const [approved, 	setApproved] = useState(false)
-	const [completed, 	setCompleted] = useState(false)
+  const render_AcivityForm = () => {
+    return (
+      <div className={"flex flex-col gap-2 w-96"}>
+        <input
+          className={inputStyle}
+          type={"text"}
+          placeholder={"Enter title"}
+          onInput={(e) => setTitle(e.target.value)}
+        />
+        <input
+          className={inputStyle}
+          type={"text"}
+          placeholder={"Enter activity type"}
+          onInput={(e) => setType(e.target.value)}
+        />
+        <input
+          className={inputStyle}
+          type={"text"}
+          placeholder={"Enter your student ID"}
+          onInput={(e) => setStudentID(e.target.value)}
+        />
+        <input
+          className={inputStyle}
+          type={"date"}
+          placeholder={"scheduled date"}
+          onInput={(e) => setDate(e.target.value)}
+        />
+        <input
+          className={inputStyle}
+          type={"text"}
+          placeholder={"Add a description"}
+          onInput={(e) => setDescp(e.target.value)}
+        />
+        <div className={"flex justify-center gap-8"}>
+          <div className={"flex items-center mb-4"}>
+            {" "}
+            <input
+              className={
+                "w-4 h-4 mx-2 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              }
+              type={"checkbox"}
+              placeholder={"approved"}
+              onInput={(e) => setApproved(e.target.value)}
+            />
+            <label> {"Approved"} </label>
+          </div>
+          <div className={"flex items-center mb-4"}>
+            {" "}
+            <input
+              className={
+                "w-4 h-4 mx-2 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              }
+              type={"checkbox"}
+              placeholder={"completed"}
+              onInput={(e) => setCompleted(e.target.value)}
+            />
+            <label> {"Completed"} </label>
+          </div>
+        </div>
+        <button className={btnStyle} onMouseDown={() => add_Activity()}>
+          {" "}
+          {"Create Activity"}{" "}
+        </button>
+      </div>
+    );
+  };
 
-	const render_AcivityForm = () => {
-		return (
-			<div className={'flex flex-col gap-2'}>
-				<h3> {'Add Activity'} </h3>
-				<input className={inputStyle} type={'text'} placeholder={'title'} onInput={e => setTitle(e.target.value)} />
-				<input className={inputStyle} type={'text'} placeholder={'type'} onInput={e => setType(e.target.value)} />
-				<input className={inputStyle} type={'text'} placeholder={'student ID'} onInput={e => setStudentID(e.target.value)} />
-				<input className={inputStyle} type={'date'} placeholder={'scheduled date'} onInput={e => setDate(e.target.value)} />
-				<input className={inputStyle} type={'text'} placeholder={'desscription'} onInput={e => setDescp(e.target.value)} />
-				<label> {'Approved'} </label>
-				<input className={inputStyle} type={'checkbox'} placeholder={'approved'} onInput={e => setApproved(e.target.value)} />
-				<label> {'Completed'} </label>
-				<input className={inputStyle} type={'checkbox'} placeholder={'completed'} onInput={e => setCompleted(e.target.value)} />
-				<button onMouseDown={() => add_Activity()}> {'Create Activity'} </button>
-			</div>
-		)
-	}
+  const add_Activity = () => {
+    const newActivity = {
+      title: title,
+      type: type,
+      studentID: studentID,
+      Scheduled_Date: date,
+      description: descp,
+      isApproved: approved,
+      isCompleted: completed,
+    };
 
-	const add_Activity = () => {
-		const newActivity = {
-			"title":			title		,
-			"type":				type		,
-			"studentID":		studentID	,
-			"Scheduled_Date":	date		,
-			"description": 		descp		,
-			"isApproved": 		approved	,
-			"isCompleted": 		completed	,
-		}
+    const newList = [...list, newActivity];
 
-		const newList = [ ...list, newActivity ]
+    console.log(newList);
 
-		console.log( newList );
+    setList(newList);
+    setAdd(false);
+  };
 
-		setList( newList )
-		setAdd( false )
-	}
+  const switch_AddActivity = () => {
+    if (!add) {
+      return (
+        <button className={btnStyle} onMouseDown={() => setAdd(true)}>
+          {" "}
+          {"Add Activity"}{" "}
+        </button>
+      );
+    } else {
+      return render_AcivityForm();
+    }
+  };
 
-	const switch_AddActivity = () => {
-
-		if ( !add ) {
-			return ( <button className={ btnStyle } onMouseDown={ () => setAdd( true )} > {'Add Activity'} </button> )
-		} else {
-			return render_AcivityForm()
-		}
-	}
-
-	return (
-		<div className={'flex flex-col'}>
-			{ switch_AddActivity() }
-			<ItemList data={ list } />
-		</div>
-	)
+  return (
+    <div className={"flex flex-col items-center"}>
+      {switch_AddActivity()}
+      <ItemList data={list} />
+    </div>
+  );
 }
 
-export default Activity
+export default Activity;
